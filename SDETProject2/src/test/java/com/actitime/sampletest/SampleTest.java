35d8fbb4b04a72fe0c690e16c1ff899e4a01b748
sandeepini <sandeepkondaiah@gmail.com>
2019-08-23 19:12:48 +0530

    my first commit

---
+++ SDETProject2/src/test/java/com/actitime/sampletest/SampleTest.java
@@ -0,0 +1,28 @@
+package com.actitime.sampletest;
+
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	
+	@Test
+	public void createUserTest(){
+		System.out.println("test1");
+	}
+	
+	
+	@Test
+	public void modifyUserTest(){
+		System.out.println("test2");
+	}
+	
+	
+	@Test
+	public void deleteUserTest(){
+		
+		
+		System.out.println("test3");
+		
+	}
+
+}
